005b196bb265d56b59ddf420cddcf870cf3cf987
Shane Charles <shanecharles@burningicesolutions.com>
2017-06-02 13:56:03 -0500

    consolidate the data processing and introduction code

--- scripts/introduction.fsx
+++ scripts/introduction.fsx
@@ -52,3 +52,36 @@ let seekKey' keys =
 seekKey' datapoints 1000
 
 List.map (seekKey' datapoints) searchKeys
+
+
+let fibonacci n =
+    let rec loop x y c = 
+        if c = n then y
+        else loop y (x+y) (c+1)
+    loop 0L 1L 1
+
+fibonacci 100
+
+let fibSeq =
+    let rec loop x y = seq {
+        yield y
+        yield! loop y (x+y)
+    }
+    loop 0L 1L
+
+fibSeq |> Seq.take 10 |> Seq.toArray
+
+let fibSeq' = Seq.unfold (fun (x,y) -> Some (y, (y, x+y))) (0L,1L) 
+
+fibSeq' |> Seq.take 10 |> Seq.toArray
+
+fibSeq |> Seq.take 10 
+  |> Seq.map (fun x -> x * x * x)
+  |> Seq.filter (fun x -> x % 2L = 0L)
+  |> Seq.toArray
+
+
+let getFibArray n = Seq.take n >> Seq.toArray
+
+(fibSeq |> getFibArray 100) = (fibSeq' |> getFibArray 100)
+
