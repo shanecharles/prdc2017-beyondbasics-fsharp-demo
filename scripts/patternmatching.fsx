5d5389bb51517284910ab1324c645baa5c93db8e
Shane Charles <shanecharles@burningicesolutions.com>
2017-06-03 05:57:53 -0500

    refactor the demo

--- scripts/patternmatching.fsx
+++ scripts/patternmatching.fsx
@@ -1,32 +1,6 @@
 open System
 
-let showSome data = 
-    match data with 
-    | Some d -> printfn "The data is: %A" d
-    | None   -> printfn "There is no data"
-
-showSome (Some "Bob")
-
-showSome None
-
-
-let showSome' = function
-    | Some d -> printfn "The data is: %A" d
-    | None   -> printfn "There is no data"
-
-showSome' (Some "Hank")
-showSome' None
-
-let greetAstronaut (name : string) =
-    match name.ToUpper() with
-    | "NEIL" -> printfn "All hail the first person to step on the moon!"
-    | "DAVE" -> printfn "I'm sorry %s, I'm afraid I can't do that." name
-    | _      -> printfn "Hi %s!" name
-
-
-greetAstronaut "dave"
-greetAstronaut "Neil"
-
+let release = (2, 2, 2)
 
 let (|RGB|) (color : System.Drawing.Color) =
     (color.R, color.G, color.B)
@@ -43,34 +17,6 @@ let averageRGBs c1 c2 =
     | RGB(r,g,b), RGB(r',g',b') -> (avg r r', avg g g', avg b b')
 
 let color2 = System.Drawing.Color.FromArgb(255,200,100,152)
-
-
-let release = (2, 2, 2)
-
-type RequiredUpdate =
-    | Major
-    | Minor
-    | Build
-    | NoUpdate
-
-let checkForUpdate (rMaj, rMin, rBld) userVer = 
-    match userVer with
-    | (uMaj, _, _) when rMaj > uMaj                                     -> Major
-    | (uMaj, uMin, _) when rMaj = uMaj && rMin > uMin                   -> Minor
-    | (uMaj, uMin, uBld) when rMaj = uMaj && rMin = uMin && rMin > uBld -> Build
-    | _                                                                 -> NoUpdate
-
-
-let testUpdate userVer =
-    let check = checkForUpdate release
-    printfn "Release: %A" release
-    printfn "User: %A" userVer
-    userVer |> check |> printfn "Update: %A" 
-
-testUpdate (1,1,1)
-testUpdate (2,1,1)
-testUpdate (2,2,1)
-testUpdate (2,2,2)
 
 
 
